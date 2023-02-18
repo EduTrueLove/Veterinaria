107a4544b54233af41148caeab6198ee1f9d3cf0
Eduardo Corona <eduardocamposcorona@gmail.com>
2023-02-17 22:08:32 -0600

    segundo dia de proyecto con registro de gatos y perros

--- src/Veterinaria/Veterinaria.java
+++ src/Veterinaria/Veterinaria.java
@@ -1,5 +1,9 @@
 package Veterinaria;
 
+import Clientes.Gato;
+import Clientes.Mascota;
+import Clientes.Perro;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Veterinaria {
@@ -9,6 +13,9 @@ public class Veterinaria {
 
 
     public static void menuPrincipal(){
+        ArrayList<Mascota> mascotasVeterinaria = new ArrayList<>();
+        Mascota mascotaTemporal;
+
         boolean menu = true;
 
         do {
@@ -32,6 +39,14 @@ public class Veterinaria {
 
             switch (opcion){
                 case 1:
+                    mascotaTemporal = menuMascotas();
+                    System.out.println(mascotaTemporal.getClass().toString());
+                    if (esPerroOEsGato(mascotaTemporal).equals("class Clientes.Gato") || esPerroOEsGato(mascotaTemporal).equals("class Clientes.Perro")){
+                        mascotasVeterinaria.add(mascotaTemporal);
+                    }else{
+                        System.out.println("Ninguna operacion fue registrada.");
+                        System.out.println();
+                    }
                     menuMascotas();
                     break;
                 case 2:
@@ -50,7 +65,7 @@ public class Veterinaria {
         }while (menu);
     }
 
-    public static void menuMascotas(){
+    public static Mascota menuMascotas(){
         boolean menu = true;
 
         do {
@@ -77,8 +92,7 @@ public class Veterinaria {
 
             switch (opcion){
                 case 1:
-                    registroMascotas();
-                    break;
+                    return registroMascotas();
                 case 2:
                     System.out.println("mostrar todo");
                     System.out.println();
@@ -106,9 +120,11 @@ public class Veterinaria {
                     break;
             }
         }while (menu);
+
+        return null;
     }
 
-    public static void registroMascotas(){
+    public static Mascota registroMascotas(){
         boolean menu = true;
 
         do {
@@ -130,13 +146,9 @@ public class Veterinaria {
 
             switch (opcion){
                 case 1:
-                    System.out.println("perro");
-                    System.out.println();
-                    break;
+                    return registrarPerro();
                 case 2:
-                    System.out.println("gato");
-                    System.out.println();
-                    break;
+                    return registrarGato();
                 case 3:
                     System.out.println("regresar menu anterior");
                     System.out.println();
@@ -147,5 +159,70 @@ public class Veterinaria {
                     System.out.println();
             }
         }while (menu);
+
+        return null;
+    }
+
+    public static Perro registrarPerro(){
+        Integer id;
+        String nombre;
+        String raza;
+        Character sexo;
+        String nombreDuenho;
+        Boolean agresivo;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa el id del perro: ");
+        id = Integer.valueOf(sc.nextLine());
+        System.out.println("Ingresa el nombre del perro: ");
+        nombre = sc.nextLine();
+        System.out.println("Ingresa la raza del perro: ");
+        raza = sc.nextLine();
+        System.out.println("Ingresa el sexo del perro (M, H): ");
+        sexo = Character.valueOf(sc.nextLine().charAt(0));
+        System.out.println("Ingresa el nombre del duenho del perro: ");
+        nombreDuenho = sc.nextLine();
+        System.out.println("Ingresa 'true' o 'false' si el perro es agresivo: ");
+        agresivo = Boolean.valueOf(sc.nextLine());
+
+
+        return new Perro(id, nombre, raza, sexo, nombreDuenho, agresivo);
+    }
+
+    public static Gato registrarGato(){
+        Integer id;
+        String nombre;
+        String raza;
+        Character sexo;
+        String nombreDuenho;
+        Boolean cuidadoEspecial;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa el id del gato: ");
+        id = Integer.valueOf(sc.nextLine());
+        System.out.println("Ingresa el nombre del gato: ");
+        nombre = sc.nextLine();
+        System.out.println("Ingresa la raza del gato: ");
+        raza = sc.nextLine();
+        System.out.println("Ingresa el sexo del gato (M, H): ");
+        sexo = Character.valueOf(sc.nextLine().charAt(0));
+        System.out.println("Ingresa el nombre del duenho del gato: ");
+        nombreDuenho = sc.nextLine();
+        System.out.println("Ingresa 'true' o 'false' si el gato tiene algun cuidado especial: ");
+        cuidadoEspecial = Boolean.valueOf(sc.nextLine());
+
+        return new Gato(id,nombre,raza,sexo,nombreDuenho,cuidadoEspecial);
+
+    }
+
+    public static String esPerroOEsGato(Mascota mascota){
+        if (mascota.getClass().toString().equals("class Clientes.Gato")) {
+            return mascota.getClass().toString();
+        }else if (mascota.getClass().toString().equals(" class Clientes.Perro")) {
+            return mascota.getClass().toString();
+        }else{
+            return null;
+        }
     }
 }
